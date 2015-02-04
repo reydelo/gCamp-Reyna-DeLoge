bc1984564ac3539734c0e6d67407b88a2168db6a
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-04 15:54:31 -0700

    add edit link to users/index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,20 @@ class UsersController<ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
