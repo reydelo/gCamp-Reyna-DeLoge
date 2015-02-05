827ff647811110864670649654d27fb1cb10bff4
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-04 17:04:10 -0700

    add delete button and button formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,13 @@ class UsersController<ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, notice: 'User was successfully destroyed.'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
