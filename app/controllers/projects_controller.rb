2f97167fb426049ca40412cdf7217141e0c42ea0
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-08 15:28:16 -0700

    user can see project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,43 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to projects_path, notice: 'Project was successfully updated.'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    if @project.destroy
+      redirect_to projects_path, notice: 'Project was successfully destroyed.'
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
 end
---
+++ db/migrate/20150208211503_create_project.rb
@@ -0,0 +1,5 @@
+class CreateProject < ActiveRecord::Migration
+  def change
+    add_column :projects, :name, :string
+  end
+end
