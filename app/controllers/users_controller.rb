5cd754d1423f013e37c22e4fe0528608915efe35
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-17 15:29:50 -0700

    validate user name and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController<ApplicationController
   def create
     @user=User.new(user_params)
     if @user.save
-      redirect_to users_path, notice: 'User was successfully created.'
+      redirect_to users_path(@user), notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -25,7 +27,9 @@ class UsersController<ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path, notice: 'User was successfully updated.'
+      redirect_to users_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
