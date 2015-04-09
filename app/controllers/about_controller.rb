0f8b2cee8b7ef3ed2300c15ed02e6fd3fea79d0d
Reyna DeLoge <reyna.deloge@gmail.com>
2015-04-08 21:09:08 -0600

    users can see gCamp stats on the about page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,11 @@
 class AboutController < ApplicationController
-  
+
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 
 end
