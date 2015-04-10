2b8ae1f11e5828c83cd7fbeb4166354825de83bf
Reyna DeLoge <reyna.deloge@gmail.com>
2015-04-10 10:35:21 -0600

    logged in user redirects to projects index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to '/', notice: 'Welcome back'
+      redirect_to projects_path, notice: 'Welcome back'
     else
       flash[:error] = "Username or password is incorrect"
       render :new
